cfe14993a694e2ab1febaeddd918a81af9e57a03
jaehoon1022 <hyunsense1022@gmail.com>
2021-11-22 13:58:08 +0900

    SENSE.GG 프로젝트 생성

---
+++ src/main/java/com/sense/SenseApplication.java
@@ -0,0 +1,13 @@
+package com.sense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sense/SenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
